c9b94384fd7de12a4df03a26932a1bc3eb327e47
muskann778 <muskannishad71@gmail.com>
2024-12-01 23:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add interactivity to buttons
+document.getElementById("cta-btn").addEventListener("click", () => {
+    alert("Welcome! Let's get started!");
+  });
+  
+  // Form Submission
+  document.getElementById("contact-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thank you for contacting us!");
+  });
+  
+  // Responsive Navbar
